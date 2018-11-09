0c2a3cdd9401ee2c6b82ea408726168787d67e6a
Benji Shankwitz <bshankwitz@gmail.com>
2018-11-09 17:28:41 -0500

    start server project, add socketio

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){  
+	console.log('a user connected');
+
+	socket.on('movement', function(data){
+		console.log('movement fired');
+
+		// sending to all clients except sender
+		socket.broadcast.emit('movement', data);
+	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on 3000');
+});
